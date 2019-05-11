62a5ec34367a4b1520f3b027ae4a933b88008e69
CangShYao <1937636639@qq.com>
2019-05-11 09:24:14 +0800

    message
    Signed-off-by: CangShYao <1937636639@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <el-menu default-active="/" class="top-nav" mode="horizontal" :router=true>
+    <el-menu class="top-nav" mode="horizontal" router :default-active='$route.path'>
       <el-menu-item index="/">
         GCTF
       </el-menu-item>
